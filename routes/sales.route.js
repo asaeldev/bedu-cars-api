db3a1c998f48e361ffd955f943f2ef837821b26e
Diegomer23 <diegomer4@gmail.com>
2022-10-09 14:12:52 -0500

    Configuración de Routes

--- routes/sales.route.js
+++ routes/sales.route.js
@@ -1,11 +1,77 @@
 const router = require('express').Router();
 const salesController = new SalesController();
 const SalesController = require('../controllers/sales.controller');
+const {
+    boomErrorHandler,
+    ormErrorHandler,
+  } = require('./middlewares/error.handler');
 
-router.get('/', salesController.all);
-router.get('/:id', salesController.findOne);
-router.post('/', salesController.create);
-router.patch('/:id', salesController.updateStatus);
-router.delete('/:id', salesController.delete);
+router.get('/', async (req, res) => {
+  const { fields } = req.query;
+  const parsedFields = fields ? fields.split(',') : null;
+  const sales = await salesController.all(parsedFields);
+  return res.status(200).json(sales);
+});
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const sale = await salesController.findOne(id);
+    return res.status(200).json(sale);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  const data = req.body;
+
+  try {
+    const sale = await salesController.create(data);
+    return res.status(201).json({
+      created: sale !== null,
+      data: sale,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/:id', async (req, res, next) => {
+  const { status } = req.body;
+  const { id } = req.params;
+
+  try {
+    const updatedRecords = await salesController.updateStatus(id, status);
+    const sale = await salesController.findOne(id);
+    return res.status(200).json({
+      updated: updatedRecords > 0,
+      data: sale,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deletedRows = await salesController.delete(id);
+    return res.status(200).json({
+      deleted: deletedRows > 0,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+
+app.listen(config.port, () => {
+  console.log('App running on port:', config.port);
+});
 
 module.exports = router;
--- routes/users.route.js
+++ routes/users.route.js
@@ -3,11 +3,124 @@ const usersController = new UsersController();
 const UsersController = require('../controllers/users.controller');
 const { signUp, logIn } = require('../controllers/users.controller');
 
-router.get('/', usersController.all);
-router.get('/:id', usersController.findOne);
-router.post('/', usersController.create);
-router.patch('/:id', usersController.update);
-router.delete('/:id', usersController.delete);
+router.get('/customers', async (req, res) => {
+  const customers = await usersController.all();
+
+  return res.status(200).json(customers);
+});
+
+router.get('/customers/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const customer = await usersController.findOne(id);
+    return res.status(200).json(customer);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/customers', async (req, res, next) => {
+  const data = req.body;
+
+  try {
+    const customer = await usersController.create(data);
+    return res.status(201).json({
+      created: true,
+      data: customer,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/customers/:id', async (req, res, next) => {
+  const data = req.body;
+  const { id } = req.params;
+
+  try {
+    const updatedRecords = await usersController.update(id, data);
+    const customer = await usersController.findOne(id);
+    return res.status(200).json({
+      updated: updatedRecords > 0,
+      data: customer,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/customers/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deletedRows = await usersController.delete(id);
+    return res.status(200).json({
+      deleted: deletedRows > 0,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Routes for administrators.
+router.get('/administrators', async (req, res) => {
+  const administrators = await usersController.all('administrator');
+  return res.status(200).json(administrators);
+});
+
+router.get('/administrators/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const administrator = await usersController.findOne(id, 'administrator');
+    return res.status(200).json(administrator);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/administrators', async (req, res, next) => {
+  const data = req.body;
+
+  try {
+    const administrator = await usersController.create(data, 'administrator');
+    return res.status(201).json({
+      created: true,
+      data: administrator,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/administrators/:id', async (req, res, next) => {
+  const data = req.body;
+  const { id } = req.params;
+
+  try {
+    const rowsUpdated = await usersController.update(id, data);
+    const administrator = await usersController.findOne(id, 'administrator');
+    return res.status(200).json({
+      updated: rowsUpdated > 0,
+      data: administrator,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/administrators/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const deleted = await usersController.delete(id, 'administrator');
+    return res.status(200).json({
+      deleted: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 router.post('/signUp', signUp);
 router.post('/logIn', logIn)
 
