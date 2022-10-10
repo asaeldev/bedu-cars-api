71a9d2b3d133c65ab55bd4f5376dd1566ea71216
Asael Hernández <asael.dev@gmail.com>
2022-10-09 23:12:03 -0500

    Installed swagger-express

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const config = require('./config/config');
 const express = require('express');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 const cors = require('cors');
 const {
   ormErrorHandler,
@@ -9,7 +13,7 @@ const routerApi = require('./routes');
 
 const app = express();
 
-const allowedDomains = ['https://localhost:8080'];
+const allowedDomains = ['https://localhost:3000'];
 
 const corsSettings = {
   origin: (origin, callback) => {
@@ -29,6 +33,30 @@ require('./utils/auth/index');
 app.get('/', (req, res) => {
   res.send('Welcome to Bedu Used Cars for Sale API!');
 });
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Bedu Used Cars for Sale API',
+      version: '1.0.0',
+      description: 'BEDU Backend Fundamentals project. Used Cars for Sale API.',
+    },
+    servers: [
+      {
+        url: 'https://bedu-cars-db.herokuapp.com/',
+      },
+    ],
+  },
+  apis: ['./routes/*.route.js'],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 routerApi(app);
 
