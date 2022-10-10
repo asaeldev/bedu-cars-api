9f1c56e93e1f4928cb8bfff340c3f4203949915b
Ricardo Hernandez <ricardo.hernandez.martinez96@gmail.com>
2022-10-09 22:55:07 -0600

    Documentacion de customers y sales

--- routes/sales.route.js
+++ routes/sales.route.js
@@ -3,6 +3,128 @@ const SalesController = require('../controllers/sales.controller');
 
 const salesController = new SalesController();
 
+/**
+ * @swagger
+ * /api/v1/sales:
+ *   get:
+ *     summary: Lists all the car sales
+ *     description: Get the entire list of cars sold
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Returns the entire list of cars sold.
+ */
+
+/**
+ * @swagger
+ * /api/v1/sales/{id}:
+ *   get:
+ *     summary: Select a specific car sale
+ *     description: Get the description of a car sale
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the sale.
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Returns the description of a car sale.
+ */
+
+/**
+ * @swagger
+ * /api/v1/sales/:
+ *   post:
+ *     summary: Create a car sale
+ *     description: Create a new car sale
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               quantity:
+ *                 type: integer
+ *                 description: Quantity of cars sold.
+ *                 example: 2
+ *               CarId:
+ *                 type: integer
+ *                 description: The Numeric ID of the Car sold.
+ *                 example: 1
+ *               UserId:
+ *                 type: integer
+ *                 description: The Numeric ID of the user that made the purchase.
+ *                 example: 3
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Registers a new car sale.
+ */
+
+/**
+ * @swagger
+ * /api/v1/sales/{id}:
+ *   patch:
+ *     summary: Update the status of a car sale
+ *     description: Update the status of a car sale
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the sale.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               status:
+ *                 type: string
+ *                 description: Status of the sale.
+ *                 example: Canceled
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Updates the status of a car sale
+ */
+
+/**
+ * @swagger
+ * /api/v1/sales{id}:
+ *   delete:
+ *     summary: Deletes a car sale
+ *     description: Deletes a car sale specified by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the sale.
+ *     requestBody:
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userName:
+ *                 type: string
+ *                 description: Name of the user that made the purchase
+ *                 example: evelyn
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Deletes a car sale.
+ */
+
+
 router.get('/', async (req, res) => {
   const { fields } = req.query;
   const parsedFields = fields ? fields.split(',') : null;
